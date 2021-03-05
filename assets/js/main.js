4072cdeba41f5c8612c0c0801f9f49ff5b272ab2
Roger Lemos <rogerlemos28@gmail.com>
2021-03-05 17:43:59 -0300

    modal blocks add

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,20 +19,16 @@ function myFunction() {
     console.log(icon.src)
   } 
 
-
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function openModal() {
+    // Get the modal
+var modal = document.getElementById("modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+ 
+modal.style.display = "block";
+ 
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -45,3 +41,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 } 
+}
