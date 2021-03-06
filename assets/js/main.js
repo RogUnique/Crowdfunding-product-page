eed1f0671cd854b40be4de35504e76beeed19e7d
Roger Lemos <rogerlemos28@gmail.com>
2021-03-06 13:55:09 -0300

    radion buttons aligned

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,24 +21,25 @@ function myFunction() {
 
 function openModal() {
     // Get the modal
-var modal = document.getElementById("modal");
+  
+  var modal = document.getElementById("modal");
+  var containerModal = document.getElementById("containerModal");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  
+  modal.style.display = "block";
+  containerModal.style.display = "block";
 
- 
-modal.style.display = "block";
- 
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
   }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
 } 
 }
